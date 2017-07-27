4bcff731963733d669ba2c425f784bd0218f50aa
Jonas Scholl <anselm.scholl@tu-harburg.de>
2017-07-27 17:20:29 +0200

    Slides and vector benchmarks.

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -10,6 +10,10 @@ import qualified ListFusion.LikeGHC as LG
 import qualified ListFusion.LikeGHCWithRules as LGR
 import qualified ListFusion.GHC as G
 
+import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as VU
+import qualified Data.Vector.Storable as VS
+
 sqr :: Int -> Int
 sqr x = x * x
 
@@ -38,6 +42,15 @@ sumSquaresNoList (from, to) = go 0 from
         go acc n | acc `seq` (n <= to) = go (acc + sqr n) (n + 1)
                  | otherwise           = acc
 
+sumSquaresVector :: (Int, Int) -> Int
+sumSquaresVector (from, to) = V.sum . V.map sqr $ V.iterateN (to - from + 1) (+1) from
+
+sumSquaresUnboxedVector :: (Int, Int) -> Int
+sumSquaresUnboxedVector (from, to) = VU.sum . VU.map sqr $ VU.iterateN (to - from + 1) (+1) from
+
+sumSquaresStorableVector :: (Int, Int) -> Int
+sumSquaresStorableVector (from, to) = VS.sum . VS.map sqr $ VS.iterateN (to - from + 1) (+1) from
+
 -- * Maximum of list, take
 
 maximumTakeNaive :: (Int, Int) -> Int
@@ -58,6 +71,15 @@ maximumTakeNoList (from, to) = to `seq` go (-1) 0
         go acc n | acc `seq` (n < from) = go (max acc ((n * n) `rem` to)) (n + 1)
                  | otherwise            = acc
 
+maximumTakeVector :: (Int, Int) -> Int
+maximumTakeVector (from, to) = V.maximum . V.map (\ x -> sqr x `rem` to) . V.take from $ V.generate (to - 0 + 1) (+0)
+
+maximumTakeUnboxedVector :: (Int, Int) -> Int
+maximumTakeUnboxedVector (from, to) = VU.maximum . VU.map (\ x -> sqr x `rem` to) . VU.take from $ VU.generate (to - 0 + 1) (+0)
+
+maximumTakeStorableVector :: (Int, Int) -> Int
+maximumTakeStorableVector (from, to) = VS.maximum . VS.map (\ x -> sqr x `rem` to) . VS.take from $ VS.generate (to - 0 + 1) (+0)
+
 -- * Maximum of list, drop
 
 maximumDropNaive :: (Int, Int) -> Int
@@ -77,6 +99,15 @@ maximumDropNoList (from, to) = to `seq` go (-1) from
     where
         go acc n | acc `seq` (n <= to) = go (max acc ((n * n) `rem` to)) (n + 1)
                  | otherwise           = acc
+
+maximumDropVector :: (Int, Int) -> Int
+maximumDropVector (from, to) = V.maximum . V.map (\ x -> sqr x `rem` to) . V.drop from $ V.generate (to - 0 + 1) (+0)
+
+maximumDropUnboxedVector :: (Int, Int) -> Int
+maximumDropUnboxedVector (from, to) = VU.maximum . VU.map (\ x -> sqr x `rem` to) . VU.drop from $ VU.generate (to - 0 + 1) (+0)
+
+maximumDropStorableVector :: (Int, Int) -> Int
+maximumDropStorableVector (from, to) = VS.maximum . VS.map (\ x -> sqr x `rem` to) . VS.take from $ VS.generate (to - 0 + 1) (+0)
 
 -- * Average
 
@@ -108,6 +139,15 @@ averageNoList (a, b, count) = go1 count 0 0
         go2 0 acc len = acc `quot` len
         go2 n acc len = (go2 (n - 1) $! (acc + b)) $! (len + 1)
 
+averageVector :: (Int, Int, Int) -> Int
+averageVector (a, b, count) = uncurry quot . V.foldl avgHelper (0, 0) $ V.replicate count a V.++ V.replicate count b
+
+averageUnboxedVector :: (Int, Int, Int) -> Int
+averageUnboxedVector (a, b, count) = uncurry quot . VU.foldl avgHelper (0, 0) $ VU.replicate count a VU.++ VU.replicate count b
+
+averageStorableVector :: (Int, Int, Int) -> Int
+averageStorableVector (a, b, count) = uncurry quot . VS.foldl avgHelper (0, 0) $ VS.replicate count a VS.++ VS.replicate count b
+
 -- * Benchmark
 
 -- | Check whether all functions actually do the same
@@ -118,19 +158,21 @@ testSame (x:xs) arg = do
         rs = map ($ arg) xs
     when (any (r /=) rs) $
         fail $ "Inconsistent results: " ++ show rs
+    putStrLn "passed"
 
 critConfig :: Config
-critConfig = defaultConfig {
-     csvFile    = Just $ "summary.csv"
-    ,reportFile = Just $ "report.html"
-}
+critConfig = defaultConfig { reportFile = Just $ "report.html" }
 
 main :: IO ()
 main = do
-    testSame [sumSquaresNaive, sumSquaresLikeGHC, sumSquaresLikeGHCWithRules, sumSquaresGHC, sumSquaresNoList] (0, 10000)
-    testSame [maximumTakeNaive, maximumTakeLikeGHC, maximumTakeLikeGHCWithRules, maximumTakeGHC, maximumTakeNoList] (5000, 10000)
-    testSame [maximumDropNaive, maximumDropLikeGHC, maximumDropLikeGHCWithRules, maximumDropGHC, maximumDropNoList] (5000, 10000)
-    testSame [averageNaive, averageLikeGHC, averageLikeGHCWithRules, averageGHC, averageNoList] (5, 10, 5000)
+    testSame [sumSquaresNaive, sumSquaresLikeGHC, sumSquaresLikeGHCWithRules, sumSquaresGHC, sumSquaresNoList,
+              sumSquaresVector, sumSquaresUnboxedVector, sumSquaresStorableVector] (0, 10000)
+    testSame [maximumTakeNaive, maximumTakeLikeGHC, maximumTakeLikeGHCWithRules, maximumTakeGHC, maximumTakeNoList,
+              maximumTakeVector, maximumTakeUnboxedVector, maximumTakeStorableVector] (5000, 10000)
+    testSame [maximumDropNaive, maximumDropLikeGHC, maximumDropLikeGHCWithRules, maximumDropGHC, maximumDropNoList,
+              maximumDropVector, maximumDropUnboxedVector, maximumDropStorableVector] (5000, 10000)
+    testSame [averageNaive, averageLikeGHC, averageLikeGHCWithRules, averageGHC, averageNoList,
+              averageVector, averageUnboxedVector, averageStorableVector] (5, 10, 5000)
     defaultMainWith critConfig
         [ bgroup "sumSquares"
             [ bench "naive"               $ whnf sumSquaresNaive            (0, 10000)
@@ -138,6 +180,9 @@ main = do
             , bench "like GHC with rules" $ whnf sumSquaresLikeGHCWithRules (0, 10000)
             , bench "GHC"                 $ whnf sumSquaresGHC              (0, 10000)
             , bench "no-list"             $ whnf sumSquaresNoList           (0, 10000)
+            , bench "vector"              $ whnf sumSquaresVector           (0, 10000)
+            , bench "u-vector"            $ whnf sumSquaresUnboxedVector    (0, 10000)
+            , bench "s-vector"            $ whnf sumSquaresStorableVector   (0, 10000)
             ]
         , bgroup "maximumTake"
             [ bench "naive"               $ whnf maximumTakeNaive            (5000, 10000)
@@ -145,6 +190,9 @@ main = do
             , bench "like GHC with rules" $ whnf maximumTakeLikeGHCWithRules (5000, 10000)
             , bench "GHC"                 $ whnf maximumTakeGHC              (5000, 10000)
             , bench "no-list"             $ whnf maximumTakeNoList           (5000, 10000)
+            , bench "vector"              $ whnf maximumTakeVector           (5000, 10000)
+            , bench "u-vector"            $ whnf maximumTakeUnboxedVector    (5000, 10000)
+            , bench "s-vector"            $ whnf maximumTakeStorableVector   (5000, 10000)
             ]
         , bgroup "maximumDrop"
             [ bench "naive"               $ whnf maximumDropNaive            (5000, 10000)
@@ -152,6 +200,9 @@ main = do
             , bench "like GHC with rules" $ whnf maximumDropLikeGHCWithRules (5000, 10000)
             , bench "GHC"                 $ whnf maximumDropGHC              (5000, 10000)
             , bench "no-list"             $ whnf maximumDropNoList           (5000, 10000)
+            , bench "vector"              $ whnf maximumDropVector           (5000, 10000)
+            , bench "u-vector"            $ whnf maximumDropUnboxedVector    (5000, 10000)
+            , bench "s-vector"            $ whnf maximumDropStorableVector   (5000, 10000)
             ]
         , bgroup "average"
             [ bench "naive"               $ whnf averageNaive            (5, 10, 5000)
@@ -159,5 +210,8 @@ main = do
             , bench "like GHC with rules" $ whnf averageLikeGHCWithRules (5, 10, 5000)
             , bench "GHC"                 $ whnf averageGHC              (5, 10, 5000)
             , bench "no-list"             $ whnf averageNoList           (5, 10, 5000)
+            , bench "vector"              $ whnf averageVector           (5, 10, 5000)
+            , bench "u-vector"            $ whnf averageUnboxedVector    (5, 10, 5000)
+            , bench "s-vector"            $ whnf averageStorableVector   (5, 10, 5000)
             ]
         ]
